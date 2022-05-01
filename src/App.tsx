f3a7b0d169490d3c57b3b4a2e51e378b722a4394
Prabu <prabu.mgks@gmail.com>
2022-05-01 17:42:16 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="min-h-screen font-montserrat text-mini"></div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        mini: "0.9375rem",
+      },
+      colors: {
+        ltrGrayishBlue: "hsl(240, 78%, 98%)",
+        ltGrayishBlue: "hsl(234, 14%, 74%)",
+        grayishBlue: "hsl(233, 13%, 49%)",
+        dkGrayishBlue: "hsl(232, 13%, 33%)",
+        primaryGradient: {
+          from: "hsl(236, 72%, 79%)",
+          to: "hsl(237, 63%, 64%)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
